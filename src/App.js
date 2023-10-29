750e967ee2665c79aa9d85f125d9d5a7c93ff300
jawhar-bali <jawhar.bali@esprit.tn>
2023-10-30 00:52:20 +0100

    done

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Posts from './components/Posts';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Reacts from './components/Reacts';
+import Groups from './components/Groups';
+import Events from './components/Events';
+import Reclamations from './components/Reclamations';
+import Users from './components/Users';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes> {/* Utilize the Routes component to define your routes */}
+          <Route path='/Posts' element={<Posts />} />
+          <Route path='/Events' element={<Events />} />
+          <Route path='/Groups' element={<Groups />} />
+          <Route path='/Reacts' element={<Reacts />} />
+          <Route path='/Reclamations' element={<Reclamations />} />
+          <Route path='/Users' element={<Users />} />
+          {/* Other routes here */}
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
